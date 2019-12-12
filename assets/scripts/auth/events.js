14e35d10abe993d0e79ad8433acfede18893f707
Derek <robinmurphy@Robins-MacBook-Pro.local>
2019-12-12 11:21:56 -0500

    Established Authentication API Functions
    Updated config.js to direct to Heroku & localhost backend
    Successful tested sign in, sign out, change password, sign out
    Created new folder 'dreamlog' to handle the nonAuth data

---
+++ assets/scripts/auth/events.js
@@ -0,0 +1,63 @@
+'use strict'
+// HANDLES EVENTS
+// const api = require('./api')
+// const getFormFields = require('../../../lib/get-form-fields')
+const ui = require('./ui')
+const api = require('./api')
+const getFormFields = require('../../../lib/get-form-fields')
+
+// API SIGN IN
+const onSignUp = event => {
+  event.preventDefault()
+
+  const form = event.target // form that was submited
+  const formData = getFormFields(form) // get that form and run it
+
+  api.signUp(formData)
+    .then(ui.onSignupSuccess)
+    .catch(ui.onSignupFailure)
+}
+
+const onSignIn = event => {
+  event.preventDefault()
+
+  const form = event.target // form that was submited
+  const formData = getFormFields(form)
+
+  api.signIn(formData)
+    .then(ui.onSigninSuccess)
+    .catch(ui.onSigninFailure)
+}
+
+const onChangePassword = event => {
+  event.preventDefault()
+
+  const form = event.target
+  const formData = getFormFields(form)
+
+  api.changePassword(formData)
+    .then(ui.onChangePasswordSuccess)
+    .catch(ui.onChangePasswordFailure)
+}
+
+const onSignOut = event => {
+  event.preventDefault()
+  // leaving out form data bc we arnt submiting data
+  api.signOut()
+    .then(ui.onSignOutSuccess)
+    .catch(ui.onSignOutFailure)
+}
+
+// to steamline the module.exports
+const addHandlers = event => {
+  $('#sign-up').on('submit', onSignUp)
+  $('#sign-in').on('submit', onSignIn)
+  $('#change-password').on('submit', onChangePassword)
+  $('#sign-out').on('submit', onSignOut)
+}
+
+// is game over
+
+module.exports = {
+  addHandlers
+}
---
+++ curl-scripts/auth/change-password.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+curl "https://oreironautapi.herokuapp.com/change-password" \
+  --include \
+  --request PATCH \
+  --header "Authorization: Token token=${TOKEN}" \
+  --header "Content-Type: application/json" \
+  --data '{
+    "passwords": {
+      "old": "'"${OLDPW}"'",
+      "new": "'"${NEWPW}"'"
+    }
+  }'
+
+echo
